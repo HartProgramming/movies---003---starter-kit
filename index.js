e2b5b73c052c4bfedaff5aa56d8718f9a53ae69c
Web3Learner <programmedhart@protonmail.com>
2022-06-28 21:28:16 -0400

    commit

--- index.js
+++ index.js
@@ -34,6 +34,11 @@ let timeoutID;
 const onInput = async event => {
      const movies  = await fetchData(event.target.value)
 
+    if(!movies.length){
+        dropdown.classList.remove("is-active");
+        return
+    }
+
      resultsWrapper.innerHTML = "";
      dropdown.classList.add("is-active");
     for(let movie of movies){
